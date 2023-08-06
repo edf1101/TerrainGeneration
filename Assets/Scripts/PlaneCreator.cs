78e4c3caec09c3d50b291c19f1fa37b23ed54085
Ed <edfillingham1@gmail.com>
2023-08-06 15:25:29 +0100

    Added Triangulation
    now can create a PDS dealunay plane

--- Assets/Scripts/PlaneCreator.cs
+++ Assets/Scripts/PlaneCreator.cs
@@ -1,12 +1,10 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using System.Diagnostics;// used for stopwatch
-
 using TriangleNet.Geometry;// TriangleNet used for triangulation
 using TriangleNet.Topology;
 
+// Code from Ed Fillingham for Terrain Generation 2 Project
 
 public class PlaneCreator : MonoBehaviour
 {
@@ -17,35 +15,105 @@ public class PlaneCreator : MonoBehaviour
     void Start()
     {
         SW.Start();// stopwatch start timing
-    
-        SW.Stop();
 
+        createPlane();  
+        SW.Stop();
         UnityEngine.Debug.Log(SW.ElapsedMilliseconds); // print to console time elapsed
 
 
     }
 
-    void createPlane()
+    private void createPlane() 
     {
-        Vector2[] pointsIn = getPoints();
+        Vector2[] pointsIn = getPoints(); // get PDS points 
 
-      
-        Polygon polygon = new Polygon();
+      //  UnityEngine.Debug.Log(pointsIn.Length); // print to console time elapsed
 
-        // Add uniformly-spaced points
 
+        Polygon polygon = new Polygon(); // create a polygon for Triangle Library and add PDS points to it
         for (int sampleNum = 0; sampleNum < pointsIn.Length; sampleNum++)
         {
             polygon.Add(new Vertex((double)pointsIn[sampleNum].x, (double)pointsIn[sampleNum].y));
         }
 
+        // turn the polygon struct into a triangulation Type: TriangleNet.Mesh
+        TriangleNet.Meshing.ConstraintOptions options = new TriangleNet.Meshing.ConstraintOptions() { ConformingDelaunay = true };
+        TriangleNet.Mesh triangleMesh = (TriangleNet.Mesh)polygon.Triangulate(options);
+
+        TriangleBin bin = new TriangleBin(triangleMesh, 100, 100, pointsIn.Length);
+
+        // need to convert TriangleNetMesh into Unity Mesh
+        UnityEngine.Mesh unityMesh= MakeMesh(triangleMesh);
+
+        GetComponent<MeshFilter>().mesh = unityMesh;
+        
+
 
     }
 
-    private Vector2[] getPoints() // takes ~15ms on MBPro in editor
+
+    private UnityEngine.Mesh MakeMesh(TriangleNet.Mesh _mesh)
+    {
+        // code taken and modified from https://github.com/Chaosed0/DelaunayUnity/
+
+        IEnumerator<Triangle> triangleEnumerator = _mesh.Triangles.GetEnumerator();
+
+       // create lists to add mesh tris and verts to before we put them into a unity mesh
+            List<Vector3> vertices = new List<Vector3>();
+            List<int> triangles = new List<int>();
+
+        for (int chunkStart = 0; chunkStart < _mesh.Triangles.Count; chunkStart += 1)
+        {
+            if (triangleEnumerator.MoveNext())// if theres another triangle to look at then move to it
+            {
+
+                // look at the current triangle and add it to unity format
+                Triangle triangle = triangleEnumerator.Current;
+
+                // For the triangles to be right-side up, they need
+                // to be wound in the opposite direction
+
+                Vector3 v0 = GetPoint3D(_mesh, triangle.vertices[2].id);
+                Vector3 v1 = GetPoint3D(_mesh, triangle.vertices[1].id);
+                Vector3 v2 = GetPoint3D(_mesh, triangle.vertices[0].id);
+
+                triangles.Add(vertices.Count);
+                triangles.Add(vertices.Count + 1);
+                triangles.Add(vertices.Count + 2);
+
+                vertices.Add(v0);
+                vertices.Add(v1);
+                vertices.Add(v2);
+
+
+            }
+        }
+
+        UnityEngine.Mesh unityMesh = new UnityEngine.Mesh(); // convert to unity format
+        unityMesh.vertices = vertices.ToArray();
+        unityMesh.triangles = triangles.ToArray();
+        unityMesh.RecalculateNormals();
+
+        return unityMesh;
+    }
+
+
+    private Vector3 GetPoint3D(TriangleNet.Mesh _mesh,int index)
+    {
+        // code taken and modified from https://github.com/Chaosed0/DelaunayUnity/
+
+        Vertex vertex = _mesh.vertices[index];
+        float elevation = 0;    //elevations[index];
+        return new Vector3((float)vertex.x, elevation, (float)vertex.y);
+    }
+
+   
+   
+
+    private Vector2[] getPoints() 
     {
 
-        List<Vector2> pdsPoints= FastPoissonDiskSampling.Sampling(new Vector2(0, 0), new Vector2(100, 100), 4); // creates list of points sampled with PDS
+        List<Vector2> pdsPoints= FastPoissonDiskSampling.Sampling(new Vector2(0, 0), new Vector2(100, 100), 2); // creates list of points sampled with PDS
 
         // need to add outline, add points at an interval of 4m along the edge
         for(int along = 0; along <= 100; along += 4)
@@ -63,3 +131,13 @@ public class PlaneCreator : MonoBehaviour
         return pdsArray;
     }
 }
+
+
+/*Timing Data
+ * 
+ * Data from M2 Pro 12 core Macbook Pro in editor on battery
+ * 
+ * 15 ms for createPlane(); when minDis for PDS is 4m
+ * 39ms when minDis is 2m
+ * 
+ */
